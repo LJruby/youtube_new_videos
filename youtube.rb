df72123b3f9a3577c0ef6a4744342eb8693122e4
LJruby <labuda.jakub@gmail.com>
2020-02-24 11:28:51 +0100

    Update youtube.rb

--- youtube.rb
+++ youtube.rb
@@ -9,15 +9,14 @@ USER_ID = "youtuber"
 
 def authorize
   client_id = Google::Auth::ClientId.from_file('./client_secrets.json')
-  token_store = Google::Auth::Stores::FileTokenStore.new(
-  :file => './tokens.yaml')
+  token_store = Google::Auth::Stores::FileTokenStore.new(:file => './tokens.yaml')
   authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
 
   credentials = authorizer.get_credentials(USER_ID)
   if credentials.nil?
     url = authorizer.get_authorization_url(base_url: OOB_URI )
     puts "Open #{url} in your browser and enter the resulting code:"
-    code = gets
+    code = STDIN.gets
     credentials = authorizer.get_and_store_credentials_from_code(
     user_id: USER_ID, code: code, base_url: OOB_URI)
   end
