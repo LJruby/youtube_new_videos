47da7f394b36e562a0838305504f02df4c808e32
LJruby <labuda.jakub@gmail.com>
2020-02-28 12:04:06 +0100

    Update youtube.rb

--- youtube.rb
+++ youtube.rb
@@ -7,25 +7,29 @@ OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
 SCOPE = 'https://www.googleapis.com/auth/youtube'
 USER_ID = "youtuber"
 
+puts "Source code: https://github.com/LJruby/youtube_new_videos/blob/master/youtube.rb"
+
 def authorize
   client_id = Google::Auth::ClientId.from_file('./client_secrets.json')
   token_store = Google::Auth::Stores::FileTokenStore.new(:file => './tokens.yaml')
   authorizer = Google::Auth::UserAuthorizer.new(client_id, SCOPE, token_store)
-
   credentials = authorizer.get_credentials(USER_ID)
   if credentials.nil?
-    url = authorizer.get_authorization_url(base_url: OOB_URI )
+    url = authorizer.get_authorization_url(base_url: OOB_URI)
     puts "Open #{url} in your browser and enter the resulting code:"
     code = STDIN.gets
-    credentials = authorizer.get_and_store_credentials_from_code(
-    user_id: USER_ID, code: code, base_url: OOB_URI)
+    credentials = authorizer.get_and_store_credentials_from_code(user_id: USER_ID, code: code, base_url: OOB_URI)
   end
   credentials
 end
 
 youtube = Google::Apis::YoutubeV3::YouTubeService.new
 youtube.client_options.application_name = "youtube_new_videos"
+youtube.client_options.application_version = "1.0.0"
 youtube.authorization = authorize
+
+puts "Client app header: "+youtube.client.default_header.to_s
+puts "Client ID: "+youtube.authorization.client_id.to_s
 
 channels = []
 token = ''
@@ -37,6 +41,8 @@ until token.nil?
   end
   token = page.next_page_token
 end
+
+puts "Number of my subscribed channels: "+channels.length.to_s
 
 videos = []
 
@@ -51,5 +57,7 @@ channels.each do |ch|
     end
     token = page.next_page_token
   end
+  puts "Channel #{ch} has got #{page.items.count} videos"
 end
-p videos
+
+puts "Video links not older than #{ARGV[0]} day(s): "+videos.to_s
